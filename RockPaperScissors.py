c0ad83c5a71d49cdaa4a5640ddf6d5e5dc4f9045
sangah <kimsangah_@naver.com>
2024-07-09 17:31:32 +0900

    개인과제 추가 도전 항목 수정

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,22 +1,42 @@
 import random
 
-random_rps = random.choice(["가위", "바위", "보"])
-s = random_rps
+def get_player():
+    while True:
+        player = input('가위, 바위, 보 중 하나를 선택하세요 : ')
+        if player not in (['가위', '바위', '보']):
+            print('유효한 입력이 아닙니다.')
+        else: 
+            return player
+        
+draw = 0
+win = 0
+lose = 0
 
-x = input("가위, 바위, 보 중 하나를 선택하세요 :")
+while True:
 
-if x not in (["가위", "바위", "보"]):
-    print("유효한 입력이 아닙니다.")
+    player = get_player()
 
-else:
-    print("플레이어 : " + str(x) + ", 컴퓨터 : " + str(s))
-    if x==s :
-        print("무승부")
-    elif x!=s:
-        if ((str(x)=="가위" and str(s)=="보") or
-            (str(x)=="바위" and str(s)=="가위") or
-            (str(x)=="보" and str(s)=="바위")):
-            print("플레이어 승리")
-        else:
-            print("컴퓨터 승리")
+    com = random.choice(['가위', '바위', '보'])
 
+    print(f"플레이어 : {player}, 컴퓨터 : {com}")
+
+    if player==com :
+        print('무승부')
+        draw += 1
+    elif player!=com:
+        if ((str(player)=='가위' and str(com)=='보') or
+            (str(player)=='바위' and str(com)=='가위') or
+            (str(player)=='보' and str(com)=='바위')):
+            print('플레이어 승리')
+            win += 1
+    else:
+        print('컴퓨터 승리')
+        lose += 1
+
+    retry = input('다시하시겠습니까? (Y/N) ').lower()
+    if retry == 'y':
+        continue
+    elif retry == 'n':
+        break
+
+print(f"승리 : {win}, 무승부 : {draw}, 패배 : {lose}")
--- test.py
+++
@@ -1,6 +0,0 @@
-
-a = 5
-b = 3
-
-def solution(a, b):
-    sum(range(min(a, b), max(a, b) + 1))
